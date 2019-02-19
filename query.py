8ee8fe3d9e841ff58d44db376ac24a62f420992c
Yiying Cheng <yiying.cheng@Yiying-Chengs-MBP13.local>
2019-02-19 14:33:13 -0800

    use a different query for object that don't have isdeleted field

--- query.py
+++ query.py
@@ -3,10 +3,12 @@ query = {}
 destination = {}
 
 
-objects = ["opportunity", "account", "campaign", "lead", "pricebook entry", "product2", "user", "user role", "case", "contact", "contract", "event", "group",
-           "lead history", "opportunity contact role", "opportunity field history", "opportunity history", "opportunity line item", "opportunity stage", "period"]
+objects_del = ["opportunity", "account", "campaign", "lead", "pricebook entry", "product2", "case", "contact", "contract", "event", "group",
+           "lead history", "opportunity contact role", "opportunity field history", "opportunity history", "opportunity line item"]
 
-sfdc_snapshot_query = """
+objects_no_del = ["user", "user role", "opportunity stage", "period"]
+
+snapshot_query = """
       SELECT * EXCEPT (ROW_NUMBER)
         FROM (SELECT *, 
                      ROW_NUMBER() OVER (PARTITION BY id ORDER BY _sdc_batched_at DESC) ROW_NUMBER
@@ -14,7 +16,23 @@ sfdc_snapshot_query = """
                WHERE isdeleted = FALSE)
        WHERE ROW_NUMBER = 1;
 """
-for obj in objects:
-    query[obj] = sfdc_snapshot_query.format(obj.title().replace(" ", ""))
+
+snapshot_query_no_del = """
+      SELECT * EXCEPT (ROW_NUMBER)
+        FROM (SELECT *, 
+                     ROW_NUMBER() OVER (PARTITION BY id ORDER BY _sdc_batched_at DESC) ROW_NUMBER
+                FROM stitch_sfdc.{})
+       WHERE ROW_NUMBER = 1;
+"""
+
+for obj in objects_del:
+    if obj not in query.keys():
+        query[obj] = snapshot_query.format(obj.title().replace(" ", ""))
+    if obj not in destination.keys():
+        destination[obj] = "_".join(obj.split())
+
+for obj in objects_no_del:
+    if obj not in query.keys():
+        query[obj] = snapshot_query_no_del.format(obj.title().replace(" ", ""))
     if obj not in destination.keys():
         destination[obj] = "_".join(obj.split())
