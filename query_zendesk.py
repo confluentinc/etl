e13c3fcf81b0156a06bcd794b6bac3a793625d8c
Yiying Cheng <yiying.cheng@Yiying-Chengs-MBP13.local>
2019-04-09 11:33:25 -0700

    added zendesk query

---
+++ query_zendesk.py
@@ -0,0 +1,268 @@
+sql = {}
+sql["ticket_data"] = """
+SELECT
+  *
+FROM (
+  SELECT
+    DISTINCT t.id AS ticket_id,
+    cust.id AS field_id,
+    cust.value AS value,
+    cust.value__bo AS value_binary,
+    updated_at,
+    RANK() OVER(PARTITION BY t.id ORDER BY _sdc_batched_at DESC) AS rn
+  FROM
+    zendesk.tickets t, UNNEST(custom_fields) AS cust)
+WHERE
+  rn = 1 -- get latest update
+"""
+sql["user"] = """
+SELECT
+  *
+FROM (
+  SELECT DISTINCT
+    id,
+    name,
+    created_at,
+    updated_at,
+    email,
+    role,
+    organization_id,
+    default_group_id as group_id,
+    time_zone,
+    suspended,
+    RANK() OVER(PARTITION BY id ORDER BY _sdc_batched_at DESC) AS rn
+  FROM
+    zendesk.users)
+WHERE
+  rn = 1 -- get latest version 
+"""
+
+sql["ticket_metric"] = """
+SELECT
+  *
+FROM (
+  SELECT DISTINCT
+    ticket_id,
+    created_at,
+    solved_at,
+    reply_time_in_minutes.calendar AS ttfr,
+    full_resolution_time_in_minutes.calendar AS ttr,
+    reopens,
+    replies,
+    requester_wait_time_in_minutes.calendar AS requester_wait_time,
+    agent_wait_time_in_minutes.calendar AS agent_wait_time,
+    RANK() OVER(PARTITION BY ticket_id ORDER BY _sdc_batched_at DESC) AS rn
+  FROM
+    zendesk.ticket_metrics
+     )
+WHERE
+  rn = 1 -- get latest version 
+"""
+
+sql['audit'] = """
+SELECT DISTINCT 
+       id,
+       ticket_id,
+       created_at,
+       author_id
+  FROM zendesk.ticket_audits
+ WHERE created_at > (SELECT MAX(created_at) FROM zendesk_v.audit)
+"""
+
+sql['change_event'] = """
+SELECT DISTINCT a.id AS audit_id,
+       e.id AS event_id,
+       e.field_name,
+       e.value,
+       e.previous_value
+ FROM zendesk.ticket_audits a, unnest(events) AS e
+WHERE a.created_at > (SELECT MAX(created_at) FROM zendesk_v.audit)
+"""
+sql['ticket_priority'] = """
+ SELECT DISTINCT ticket_id,
+        value AS priority
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 33471847
+  AND valus IS NOT NULL
+"""
+
+sql['ticket_time_spent'] = """
+SELECT a.ticket_id,
+       MAX(c.value) AS time_spent
+  FROM zendesk_v.change_event c
+  JOIN zendesk_v.audit a
+    ON c.audit_id = a.id
+ WHERE c.field_name = '34347708'
+ GROUP BY 1
+ """
+
+sql['ticket_component'] = """
+SELECT DISTINCT ticket_id,
+       value AS component
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 33020448
+   AND valus IS NOT NULL
+"""
+
+sql["ticket_cause"] = """
+SELECT DISTINCT ticket_id,
+       value AS cause
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 47641647
+  AND valus IS NOT NULL
+"""
+
+sql['ticket_kafka_version'] = """
+SELECT DISTINCT ticket_id,
+       value AS kafka_version
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 24843497
+  AND valus IS NOT NULL
+"""
+
+sql['ticket_java_version'] = """
+SELECT DISTINCT ticket_id,
+       value AS java_version
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 26235617
+  AND valus IS NOT NULL
+"""
+
+sql['ticket_operating_system'] = """
+SELECT DISTINCT ticket_id,
+       value AS operating_system
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 26235607
+  AND valus IS NOT NULL
+"""
+
+sql['bundle_usage'] = """
+SELECT DISTINCT ticket_id,
+       value_binary AS bundle_usage
+  FROM zendesk_v.ticket_data
+ WHERE field_id = 360000036206
+  AND valus IS NOT NULL
+"""
+
+sql['ticket_initial_priority'] = """
+SELECT DISTINCT first.ticket_id,
+       e.previous_value AS initial_priority
+  FROM zendesk_v.change_event e
+  JOIN 
+	   (SELECT a.ticket_id,
+		       MIN(event_id) AS first_changed
+		  FROM zendesk_v.change_event e
+		  JOIN zendesk_v.audit a
+		    ON e.audit_id = a.id
+		 WHERE e.field_name = '33471847'
+		   AND e.previous_value > ""
+		 GROUP BY 1) first
+    ON first.first_changed = e.event_id
+"""
+
+sql['ticket'] = """
+SELECT
+  DISTINCT tickets.id,
+  organizations.name AS organization,
+  user.name AS assignee,
+  tickets.created_at,
+  tickets.updated_at,
+  CASE tickets.recipient
+    WHEN 'support-escalations@confluent.zendesk.com' THEN "escalation"
+    WHEN 'escalations@confluent.io' THEN "escalation"
+    WHEN 'se-support@confluent.io' THEN "se-support"
+    WHEN 'se-support@confluent.zendesk.com' THEN "se-support"
+    WHEN 'support@confluent.io' THEN "enterprise"
+    WHEN 'support@confluent.zendesk.com' THEN "enterprise"
+    WHEN 'cloud-support@confluent.zendesk.com' THEN "cloud-professional"
+    WHEN 'cloud-support@confluent.io' THEN "cloud-professional"
+    ELSE "enterprise"
+  END AS type,
+  tickets.status,
+  tickets.subject,
+  priority.priority,
+  ifnull(ip.initial_priority, priority.priority) initial_priority,
+  component.component,
+  time.time_spent,
+  cause.cause,
+  kversion.kafka_version,
+  bundle.bundle_usage,
+  metric.ttfr,
+  metric.ttr,
+  metric.solved_at,
+  metric.agent_wait_time,
+  metric.requester_wait_time
+FROM
+   (SELECT * FROM (
+	   SELECT *,
+	   		  RANK() OVER(PARTITION BY id ORDER BY _sdc_batched_at DESC) AS rn
+	     FROM zendesk.tickets)
+	    WHERE rn = 1) AS tickets
+LEFT JOIN
+  zendesk.organizations
+ON
+  tickets.organization_id = organizations.id
+LEFT JOIN
+  zendesk_v.user
+ON
+  tickets.assignee_id = user.id
+LEFT JOIN
+  zendesk_v.ticket_priority priority
+ON
+  tickets.id = priority.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_initial_priority ip
+ON
+  tickets.id = ip.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_component component
+ON
+  tickets.id = component.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_cause cause
+ON
+  tickets.id = cause.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_kafka_version kversion
+ON
+  tickets.id = kversion.ticket_id
+LEFT JOIN
+  zendesk_v.bundle_usage bundle
+ON
+  tickets.id = bundle.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_metric metric
+ON
+  tickets.id = metric.ticket_id
+LEFT JOIN
+  zendesk_v.ticket_time_spent time
+ON
+  tickets.id = time.ticket_id
+"""
+
+# select bundle_usage, count(*) as cnt
+# from (
+# SELECT ticket_id,
+#        value AS bundle_usage
+#   FROM zendesk_v.ticket_data
+#  WHERE field_id = 360000036206
+# )
+# group by 1
+# order by 2 desc
+
+# SELECT
+#   *
+# FROM (
+#   SELECT
+#     DISTINCT t.id AS ticket_id,
+#     cust.id AS field_id,
+#     cust.value AS value,
+#     cust.value__bo AS value_binary,
+#     updated_at,
+#     RANK() OVER(PARTITION BY t.id ORDER BY updated_at DESC) AS rn
+#   FROM
+#     zendesk.tickets t, UNNEST(custom_fields) AS cust)a
+# WHERE
+#   rn = 1
+#   AND ticket_id IN (13371)
+#     order by 1,2
---
+++ utility.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import json
+import os
+
+def setup_gbp(config_path):
+    """Set up GBQ connection
+    :param config_path: path to json file which stores BQ's credentials
+    :return: client object
+    """
+    config_file_path=os.path.expanduser(config_path)
+
+    with open(config_file_path) as f:
+        my_config = f.readline()[:-1]
+    parsed_config = json.loads(my_config)
+    print (parsed_config)
+
+    user=parsed_config["user"]
+    bq_credentials=parsed_config["bq_credentials"]
+    bq_project = parsed_config["bq_project"]
+    bq_dataset = parsed_config["bq_dataset"]
+
+    ### establish query client
+    service_account_file_path =os.path.expanduser(bq_credentials)
+    with open(service_account_file_path) as source:
+        info = json.load(source)
+
+    bq_credentials = service_account.Credentials.from_service_account_info(info)
+    project_id = bq_project
+    client = bigquery.Client(credentials=bq_credentials, project=project_id)
+    print ("Conection setup complete")
+    return client
+
+def query_to_bq(client, query, destination):
+    """Store the query result into a Google BigQuery table.
+    :param client: bigquery client
+    :param query: the query
+    :param destination_table: Name of table to be written, in the form of dataset.tablename.
+    :return: None
+    """
+    job_config = bigquery.QueryJobConfig()
+    # Set the destination table
+    dest = destination.split('.')
+    table_ref = client.dataset(dest[0]).table(dest[1])
+    job_config.destination = table_ref
+    job_config.write_disposition = "WRITE_TRUNCATE"
+    query_job = client.query(query, job_config = job_config)
+    query_job.result()
+    print("Query results loaded to table {}".format(table_ref.path))
+    
+def load_data_from_file(client, dataset_id, table_id, source_file_name):
+    """Create a table in BigQuery from a local file
+    :param client: bigquery client
+    :param dataset_id: dataset name for destination table
+    :param table_id: destination table name
+    :param source_file_name: file name
+    :return: None
+    """
+    dataset_ref = client.dataset(dataset_id)
+    table_ref = dataset_ref.table(table_id)
+
+    with open(source_file_name, 'rb') as source_file:
+        # This example uses CSV, but you can use other formats.
+        # See https://cloud.google.com/bigquery/loading-data
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = 'text/csv'
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.autodetect=True
+        job_config.skip_leading_rows = 1
+        job = client.load_table_from_file(
+            source_file, table_ref, job_config=job_config)
+
+    job.result()  # Waits for job to complete
+
+    print('Loaded {} rows into {}:{}.'.format(
+        job.output_rows, dataset_id, table_id))
+
+
+def df_to_bq(client, df, destination_table):
+    """Write a DataFrame to a Google BigQuery table.
+    :param client: bigquery client
+    :param df: the dataframe name to be copied
+    :param destination_table: Name of table to be written, in the form dataset.tablename.
+    :return: None
+    """
+    file_name = 'file.csv'
+    dest = destination_table.split('.')
+    df.to_csv(file_name, sep = ",", index = False, header = True)
+    load_data_from_file(client, dest[0], dest[1], file_name)
+
