2c1fe2854a4a4dde706128cd62eab2490dee560e
Yiying Cheng <yiying.cheng@Yiying-Chengs-MBP13.local>
2019-05-29 17:29:16 -0700

    added ticket metrics

--- zendesk_etl.py
+++ zendesk_etl.py
@@ -14,8 +14,8 @@ print(datetime.now())
 client = setup_gbp('~/.confluentR.config')
 
 incremental_table = ["audit", "change_event"]
-full_table = ["user", "ticket_data", "ticket_priority", "ticket_initial_priority","ticket_time_spent",
-			  "ticket_component", "ticket_cause", "bundle_usage", "ticket_kafka_version",
+full_table = ["user", "ticket_metric", "ticket_data", "ticket_priority", "ticket_initial_priority",
+			  "ticket_time_spent", "ticket_component", "ticket_cause", "bundle_usage", "ticket_kafka_version",
 			  "ticket_java_version", "ticket_operating_system","ticket", "satisfaction_rating",
 			  "ticket_csat"]
 # Insert newly appended records from stitch audit table to audit and change_event
